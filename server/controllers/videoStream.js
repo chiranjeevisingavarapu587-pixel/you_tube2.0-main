ca6cabe52ca6859ca4720983ed7844e5bce4978d
Chiranjeevi <chiranjeevisingavarapu@gmail.com>
2026-08-25 12:37:49 +0530

    Fix video streaming

--- server/controllers/videoStream.js
+++ server/controllers/videoStream.js
@@ -1,7 +1,18 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const streamVideo = (req, res) => {
-  const filePath = path.join(process.cwd(), "uploads", req.params.filename);
+  const filePath = path.join(
+    __dirname,
+    "../uploads",
+    req.params.filename
+  );
+
   if (
     req.params.filename.endsWith(".jpg") ||
     req.params.filename.endsWith(".jpeg") ||
@@ -9,27 +20,40 @@ export const streamVideo = (req, res) => {
   ) {
     return res.sendFile(filePath);
   }
+
   const stat = fs.statSync(filePath);
   const fileSize = stat.size;
   const range = req.headers.range;
+
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-");
+
     const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize - 1;
+
     const chunkSize = end - start + 1;
-    const file = fs.createReadStream(filePath, { start, end });
+
+    const file = fs.createReadStream(filePath, {
+      start,
+      end,
+    });
+
     res.writeHead(206, {
       "Content-Range": `bytes ${start}-${end}/${fileSize}`,
       "Accept-Ranges": "bytes",
       "Content-Length": chunkSize,
       "Content-Type": "video/mp4",
     });
+
     file.pipe(res);
   } else {
     res.writeHead(200, {
       "Content-Length": fileSize,
       "Content-Type": "video/mp4",
     });
+
     fs.createReadStream(filePath).pipe(res);
   }
 };
